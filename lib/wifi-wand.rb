40138cbe5e7cfc25b44d4ef35564b92f87cb2039
Keith Bennett <keithrbennett@gmail.com>
2018-03-14 16:14:17 +0700

    Fix models not being loadable after requiring the gem.

--- lib/wifi-wand.rb
+++ lib/wifi-wand.rb
@@ -1,3 +1,8 @@
 require_relative 'wifi-wand/version'
 
 require_relative 'wifi-wand/main'  # recursively requires the other files
+#
+# When additional operating systems are added, we will need to modify this
+# to load only the model appropriate for the environment:
+
+require_relative 'wifi-wand/models/mac_os_model'
