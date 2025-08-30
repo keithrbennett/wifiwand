1e1cefacf081f50ef705c9a0f1781e4c241394cb
keithrbennett <keithrbennett@gmail.com>
2025-08-31 01:20:13 +0800

    Add tests, NetworkStateManager is now 100% covered.

--- spec/wifi-wand/services/network_state_manager_spec.rb
+++ spec/wifi-wand/services/network_state_manager_spec.rb
@@ -33,6 +33,14 @@ describe WifiWand::NetworkStateManager do
       expect(state[:network_name]).to be_nil
       expect(state[:network_password]).to be_nil
     end
+
+    it 'handles password retrieval failure' do
+      allow(mock_model).to receive(:connected_network_name).and_return('TestNetwork')
+      allow(mock_model).to receive(:preferred_network_password).and_return(nil)
+      state = state_manager.capture_network_state
+      expect(state[:network_name]).to eq('TestNetwork')
+      expect(state[:network_password]).to be_nil
+    end
   end
 
   describe '#restore_network_state' do
@@ -104,6 +112,65 @@ describe WifiWand::NetworkStateManager do
       allow(mock_model).to receive(:wifi_on?).and_return(true)
       allow(mock_model).to receive(:connected_network_name).and_return('TestNetwork')
       expect(state_manager.restore_network_state(valid_state)).to eq(:already_connected)
+    end
+
+    it 'turns on WiFi when currently off but should be on' do
+      wifi_off_state = valid_state.merge(wifi_enabled: true)
+      allow(mock_model).to receive(:wifi_on?).and_return(false)
+      allow(mock_model).to receive(:connected_network_name).and_return('TestNetwork')
+      
+      expect(mock_model).to receive(:wifi_on)
+      expect(mock_model).to receive(:till).with(:on, WifiWand::TimingConstants::WIFI_STATE_CHANGE_WAIT)
+      
+      state_manager.restore_network_state(wifi_off_state)
+    end
+
+    it 'turns off WiFi when currently on but should be off and returns early' do
+      wifi_off_state = valid_state.merge(wifi_enabled: false)
+      allow(mock_model).to receive(:wifi_on?).and_return(true)
+      
+      expect(mock_model).to receive(:wifi_off)
+      expect(mock_model).to receive(:till).with(:off, WifiWand::TimingConstants::WIFI_STATE_CHANGE_WAIT)
+      expect(mock_model).not_to receive(:connect)
+      
+      state_manager.restore_network_state(wifi_off_state)
+    end
+
+    it 'uses fallback password when state password is nil' do
+      state_without_password = valid_state.merge(network_password: nil)
+      allow(mock_model).to receive(:wifi_on?).and_return(true)
+      allow(mock_model).to receive(:connected_network_name).and_return('OtherNetwork')
+      allow(mock_model).to receive(:preferred_network_password).with('TestNetwork').and_return('fallback_pass')
+      
+      expect(mock_model).to receive(:connect).with('TestNetwork', 'fallback_pass')
+      expect(mock_model).to receive(:till).with(:conn, WifiWand::TimingConstants::NETWORK_CONNECTION_WAIT)
+      
+      state_manager.restore_network_state(state_without_password)
+    end
+
+    it 'skips network connection when WiFi should be disabled' do
+      wifi_disabled_state = {
+        wifi_enabled: false,
+        network_name: 'TestNetwork',
+        network_password: 'testpass',
+        interface: 'wlan0'
+      }
+      allow(mock_model).to receive(:wifi_on?).and_return(false)
+      
+      expect(mock_model).not_to receive(:connect)
+      expect(mock_model).not_to receive(:wifi_on)
+      expect(mock_model).not_to receive(:wifi_off)
+      
+      state_manager.restore_network_state(wifi_disabled_state)
+    end
+
+    it 'skips network connection when no network name in state' do
+      state_no_network = valid_state.merge(network_name: nil)
+      allow(mock_model).to receive(:wifi_on?).and_return(true)
+      
+      expect(mock_model).not_to receive(:connect)
+      
+      state_manager.restore_network_state(state_no_network)
     end
 
   end
