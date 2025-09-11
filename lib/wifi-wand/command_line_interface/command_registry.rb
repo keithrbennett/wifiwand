fa4ed46a6f088522e76ef9acff68e4944107518c
keithrbennett <keithrbennett@gmail.com>
2025-09-11 20:24:29 +0800

    Remove 'l' (ls_avail_nets) command, it is no longer operational.

--- lib/wifi-wand/command_line_interface/command_registry.rb
+++ lib/wifi-wand/command_line_interface/command_registry.rb
@@ -14,7 +14,6 @@ module WifiWand
             Command.new('f',   'forget',        -> (*options)  { cmd_f(*options)   }),
             Command.new('h',   'help',          -> (*_options) { cmd_h             }),
             Command.new('i',   'info',          -> (*_options) { cmd_i             }),
-            Command.new('l',   'ls_avail_nets', -> (*_options) { cmd_l             }),
             Command.new('na',  'nameservers',   -> (*options)  { cmd_na(*options)  }),
             Command.new('ne',  'network_name',  -> (*_options) { cmd_ne            }),
             Command.new('of',  'off',           -> (*_options) { cmd_of            }),
